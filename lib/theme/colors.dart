07620113e4a6ba42419175373f0efce03316df81
bluemoon983 <msdrobot@naver.com>
2025-03-05 14:18:43 +0900

    color

--- lib/theme/colors.dart
+++ lib/theme/colors.dart
@@ -5,4 +5,16 @@ class AppColor {
   static Color main = Color(0xFF2756F1);
   static Color error = Color(0xFFDF454A);
   static Color white = Color(0xFFFFFFFF);
+  static Color black = Color(0xFF000000);
+  static Color mainChartDown = Color(0xFFFF5c5c);
+  static Color mainChartUp = Color(0xFF2196F3);
+  static Color gray800 = Color(0xFF333336);
+  static Color gray700 = Color(0xFF4D4D4F);
+  static Color gray600 = Color(0xFF666669);
+  static Color gray500 = Color(0xFF808083);
+  static Color gray400 = Color(0xFFA5A6A9);
+  static Color gray300 = Color(0xFFB4B5B7);
+  static Color gray200 = Color(0xFFCBCCCE);
+  static Color gray100 = Color(0xFFEFF0F2);
+  static Color gray50 = Color(0xFFF5F6F8);
 }
