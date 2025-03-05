f0e782e214b483c3ec2d77ca04bf3aa7728c474f
bluemoon983 <msdrobot@naver.com>
2025-03-05 13:58:57 +0900

    first commit

---
+++ lib/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+// Color 값은 0x로 시작하고 FF(투명도)를 포함한 8자리 16진수여야 합니다.
+class AppColor {
+  static Color main = Color(0xFF2756F1);
+  static Color error = Color(0xFFDF454A);
+  static Color white = Color(0xFFFFFFFF);
+}
