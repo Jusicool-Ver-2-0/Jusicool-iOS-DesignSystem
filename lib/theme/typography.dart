f9667da0c37deff8173fa81b1a1f26e8f62c339e
bluemoon983 <msdrobot@naver.com>
2025-03-05 16:28:41 +0900

    font

---
+++ lib/theme/typography.dart
@@ -0,0 +1,70 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+
+class AppTypography {
+  static TextStyle titleLarge = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w700,
+    fontSize: 48,
+    height: 1.208,
+    letterSpacing: 0,
+  );
+
+  static TextStyle titleMedium = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 36,
+    height: 1.194,
+    letterSpacing: 0,
+  );
+  static TextStyle titleSmall = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 24,
+    height: 1.292,
+    letterSpacing: 0,
+  );
+
+  static TextStyle subTitle = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 18,
+    height: 1.5,
+    letterSpacing: 0,
+  );
+
+  static TextStyle bodyLarge = TextStyle();
+
+  static TextStyle bodyMedium = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 16,
+    height: 1.375,
+    letterSpacing: 0,
+  );
+
+  static TextStyle bodySmall = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 16,
+    height: 1.375,
+    letterSpacing: 0,
+  );
+
+  static TextStyle label = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w400,
+    fontSize: 12,
+    height: 1.333,
+    letterSpacing: 0,
+  );
+
+  static TextStyle navi = TextStyle(
+    fontFamily: 'Pretendard',
+    fontWeight: FontWeight.w600,
+    fontSize: 8,
+    height: 1.5,
+    letterSpacing: 0,
+  );
+}
